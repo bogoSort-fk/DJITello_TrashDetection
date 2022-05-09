ff375766e866f507ab53ca509287b27809c778d8
Ferdi Kandemir <bogosort.f@gmail.com>
2022-05-09 18:47:37 +0300

    Center dots of detected objects can be represented in a blank image with given positions from Yolo v5 detection output.

--- deneme.py
+++ deneme.py
@@ -13,7 +13,7 @@ txt_path = r'G:\telloDeneme\sonuclar\exp13\labels\d2'
 with open(txt_path + '.txt', 'r') as f:
     d = f.readlines()
     print(d)
-characters_to_remove = '() '
+characters_to_remove = '\n() '
 
 newAL = list()
 for line in d:
@@ -22,17 +22,32 @@ for line in d:
         new_str = new_str.replace(characters,"")
     newAL.append(new_str)
 
-print(newAL[1])
+print(newAL)
 
 
+print("intliszt: " + newAL[0][0])
 
+intList = list()
+for position in newAL:
 
-# d.replace('\n', '')
-# print(d)
-# img = cv.imread(r'.\denemeler\d2.jpg')
-# dimensions = img.shape
-# blank_image = 255*np.ones(shape=dimensions,dtype=np.uint8)
-# cv.imshow('asdf',blank_image)
+    intList.append(position.split(','))
+for i in range(0, len(intList)):
+    #print(intList[i])
+    for j in range(0, len(intList[i])):
+        intList[i][j] = int(intList[i][j])
+print(intList)
+
+img = cv.imread(r'.\denemeler\d2.jpg')
+dimensions = img.shape
+cv.imshow('fafdsg',img)
+blank_image = 255*np.ones(shape=dimensions,dtype=np.uint8)
+cv.imshow('asdf',blank_image)
+
+for point in intList:
+    cv.circle(blank_image, point,6,(0,0,255), cv.FILLED)
+
+cv.imshow('sdagasg', blank_image)
+
 #
 # image_with_rect = cv.rectangle(img=blank_image,pt1=(613, 426), pt2=(351, 438),color=(255,0,0),thickness=4)
 # cv.imshow('sdfds',image_with_rect)
